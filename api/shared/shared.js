e587cf7101e3430b1c0d4ae2190a9dc8e2e98ae1
Olivier Leplus <leplus.olivier@gmail.com>
2021-08-09 09:45:37 +0200

    feat(db): Add MongoDB connection

--- api/shared/shared.js
+++ api/shared/shared.js
@@ -2,9 +2,9 @@ const mongoClient = require("mongodb").MongoClient;
 
 
 const connectDb = async () => {
-    const client = await mongoClient.connect("YOUR-CONNECTION-STRING");
+    const client = await mongoClient.connect("mongodb://thunderstormdemo4n0q4gmo:yRur0xqtU3CSKFOcuSd4qrofEcT3v8iO86LaGuyxu0sxCMZ3fAets1k0QvJbSFo8Ks4BgWs7R7YRqDtwMfDNiw%3D%3D@thunderstormdemo4n0q4gmo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@thunderstormdemo4n0q4gmo@");
   
-    return client.db("swaworkshop");
+    return client.db("thunderstormdemo4n0q4gmo ");
 };
 
 const getUser = (req) => {
